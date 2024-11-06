95789b702a4c1330b0e52a1db5617c46630e454d
JiggyJoggy <JaggerVanW@gmail.com>
2024-11-06 04:45:21 -0600

    Added audio visualizers

---
+++ beat-detection/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : AudioSyncer
+{
+    public void Update()
+    {
+        
+    }
+}
---
+++ beat-detection/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MainMenu
+{
+    public void NewSong()
+    {
+
+    }
+}
